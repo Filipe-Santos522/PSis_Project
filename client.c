592512442dccd94ef9233463fa3b848d9fe4c2fa
Filipe <filipesantos511@gmail.com>
2021-12-29 17:03:48 +0000

    start

---
+++ client.c
@@ -0,0 +1,14 @@
+#include <ncurses.h>
+#include <sys/types.h>
+#include <sys/socket.h> 
+#include <sys/un.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+#include "single-pong.h"
+
+int main(int argc, char* argv){
+    
+}
---
+++ single-pong.h
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <ncurses.h>
+
+#include "pong.h"
+
+WINDOW * message_win;
+
+typedef struct ball_position_t{
+    int x, y;
+    int up_hor_down; //  -1 up, 0 horizontal, 1 down
+    int left_ver_right; //  -1 left, 0 vertical,1 right
+    char c;
+} ball_position_t;
+
+typedef struct paddle_position_t{
+    int x, y;
+    int length;
+} paddle_position_t;
+
+typedef struct message{
+    int type; //1 - connect, 2 - release_ball, 3 - send_ball, 4 - move_ball, 5 - disconnect
+    ball_position_t* ball;
+};
+
+
+void new_paddle (paddle_position_t * paddle, int legth);
+void draw_paddle(WINDOW *win, paddle_position_t * paddle, int delete);
+void moove_paddle (paddle_position_t * paddle, int direction);
+void place_ball_random(ball_position_t * ball);
+void moove_ball(ball_position_t * ball);
+void draw_ball(WINDOW *win, ball_position_t * ball, int draw);
